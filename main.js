e3c89520f8de85a39d64c2271fcf2620db763c6b
Aryah <ar341476@dal.ca>
2023-01-26 04:36:21 -0400

    spinning cube

--- main.js
+++ main.js
@@ -1,23 +1,38 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
+import * as THREE from './node_modules/three/build/three.module.js';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 0.1, 2000);
+camera.position.z = 5;
+const renderer = new THREE.WebGLRenderer({antialias: true});
 
-setupCounter(document.querySelector('#counter'))
+renderer.setClearColor("#65647C");
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+window.addEventListener('resize', () => {
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateMatrix();
+    console.log("hello");
+})
+
+
+const boxGeometry = new THREE.BoxGeometry(2, 2, 2);
+const boxMaterial = new THREE.MeshLambertMaterial({color: "#FFF6BD"});
+const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
+boxMesh.rotation.set(40, 0, 40);
+scene.add(boxMesh);
+
+const rendering = () => {
+    requestAnimationFrame(rendering);
+    boxMesh.rotation.z -= 0.005;
+    boxMesh.rotation.x -= 0.01;
+
+    renderer.render(scene, camera);
+}
+
+const light = new THREE.PointLight(0xFFFFFF, 1, 100);
+light.position.set(3, 3, 3);
+scene.add(light);
+
+rendering();
