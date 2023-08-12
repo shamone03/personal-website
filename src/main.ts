ee9fa1ec7f69db7863820f3cf00ae25bb9ee8dce
Aryah Kannan <68197305+shamone03@users.noreply.github.com>
2023-08-12 02:47:51 -0300

    added tree models

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,19 @@
 import * as THREE from "three";
-import Stats from 'stats.js';
 import { TrackballControls } from 'three/examples/jsm/controls/TrackballControls.js';
-import { cloneUniformsGroups } from "three/src/renderers/shaders/UniformsUtils.js";
-const BOX_COLOR = "#22A39F";
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import Stats from 'stats.js';
 const SCENE_COLOR = "#F3EFE0";
 const LIGHT_COLOR = "#FFFFFF";
 
 const scene = new THREE.Scene();
 const stats = new Stats();
+const loader = new GLTFLoader();
 stats.showPanel(0);
-document.body.appendChild(stats.dom);
+// document.body.appendChild(stats.dom);
 const axesHelper = new THREE.AxesHelper(100);
 // scene.add(axesHelper)
 const camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 0.1, 2000);
-camera.position.z = 5;
+camera.position.z = 10;
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 
 renderer.setClearColor(SCENE_COLOR);
@@ -27,9 +27,9 @@ window.addEventListener('resize', () => {
 })
 
 const boxGeometry = new THREE.BoxGeometry(2, 2, 2).toNonIndexed();
-const boxMaterial = new THREE.MeshNormalMaterial();
+const boxMaterial = new THREE.MeshLambertMaterial({ color: "lightgreen", wireframe: false });
 const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
-boxMesh.rotation.set(45, 0, 45);
+boxMesh.rotation.set(0, 0, 0);
 boxMesh.name = "mainBox";
 scene.add(boxMesh);
 
@@ -41,10 +41,10 @@ controls.noZoom = true;
 
 const lighting = () => {
 	const lightData = [
-		{ color: 0xFFFFFF, intensity: 0.5, dist: 100, position: { x: 3, y: 3, z: 3 } },
-		{ color: 0xFFFFFF, intensity: 1, dist: 100, position: { x: -3, y: -3, z: 3 } },
-		{ color: 0xFFFFFF, intensity: 0.5, dist: 100, position: { x: 3, y: -3, z: -3 } },
-		{ color: 0xFFFFFF, intensity: 1, dist: 100, position: { x: -3, y: 3, z: -3 } },
+		{ color: 0xFFFFFF, intensity: 0.5, dist: 100, position: { x: 10, y: 10, z: 10 } },
+		{ color: 0xFFFFFF, intensity: 1, dist: 100, position: { x: -10, y: -10, z: 10 } },
+		{ color: 0xFFFFFF, intensity: 0.5, dist: 100, position: { x: 10, y: -10, z: -10 } },
+		{ color: 0xFFFFFF, intensity: 1, dist: 100, position: { x: -10, y: 10, z: -10 } },
 	]
 
 	for (let i = 0; i < lightData.length; i++) {
@@ -66,6 +66,9 @@ window.addEventListener('pointermove', (e) => {
 
 });
 
+loader.load(`../models/tree${Math.ceil((Math.random() * 6) / 2)}.gltf`, (gltf) => {
+	scene.add(gltf.scene);
+});
 
 const rendering = () => {
 	stats.begin();
