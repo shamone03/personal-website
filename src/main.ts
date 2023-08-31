29956b929c055de31b183e3b14bb816e25e931c7
Aryah Kannan <68197305+shamone03@users.noreply.github.com>
2023-08-31 04:04:23 -0300

    raycasting

--- src/main.ts
+++ src/main.ts
@@ -14,9 +14,10 @@ stats.showPanel(0);
 // document.body.appendChild(stats.dom);
 const axesHelper = new THREE.AxesHelper(100);
 // scene.add(axesHelper)
+const raycaster = new THREE.Raycaster();
 const camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 0.1, 2000);
-camera.position.z = 10;
 const renderer = new THREE.WebGLRenderer({ antialias: true });
+camera.position.z = 10;
 
 renderer.setClearColor(SCENE_COLOR);
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -28,11 +29,11 @@ window.addEventListener('resize', () => {
 	camera.updateProjectionMatrix();
 })
 
-const boxGeometry = new THREE.BoxGeometry(2, 2, 2).toNonIndexed();
-const boxMaterial = new THREE.MeshLambertMaterial({ color: "lightgreen", wireframe: false });
-const normMaterial = new THREE.MeshNormalMaterial();
+const boxGeometry = new THREE.BoxGeometry(2, 2, 2);
+const boxMaterial = new THREE.MeshLambertMaterial({ color: "lightgreen" });
 const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
-boxMesh.rotation.set(0, 0, 0);
+boxMesh.position.x -= 2;
+// boxMesh.position.y += 2
 boxMesh.name = "mainBox";
 scene.add(boxMesh);
 
@@ -41,6 +42,23 @@ controls.dynamicDampingFactor = 0.1;
 controls.rotateSpeed = 4;
 controls.noZoom = true;
 
+function drawLine(start: THREE.Vector3, end: THREE.Vector3) {
+	const mat = new THREE.LineBasicMaterial({color: 0xFFFFFF});
+	const lineGeometry = new THREE.BufferGeometry().setFromPoints([start, end]);
+	const line = new THREE.Line(lineGeometry, mat);
+	// scene.add(line);
+	return line;
+}
+
+function makeBoid() {
+	const normMaterial = new THREE.MeshNormalMaterial();
+	const boidMesh = new THREE.Mesh(new THREE.ConeGeometry, normMaterial);
+	boidMesh.rotateZ(90 * (Math.PI / 180));
+	boidMesh.scale.x = 0.5;
+	boidMesh.scale.y = 1.5;
+	boidMesh.scale.z = 0.5;
+	return boidMesh;
+}
 
 const lighting = () => {
 	const lightData = [
@@ -62,7 +80,7 @@ const lighting = () => {
 	}
 }
 const pointer = new THREE.Vector2();
-const raycaster = new THREE.Raycaster();
+
 window.addEventListener('pointermove', (e) => {
 	pointer.x = (e.clientX / window.innerWidth) * 2 - 1;
 	pointer.y = - (e.clientY / window.innerHeight) * 2 + 1;
@@ -70,20 +88,36 @@ window.addEventListener('pointermove', (e) => {
 });
 
 loader.load(`/models/tree${Math.ceil((Math.random() * 6) / 2)}.gltf`, (gltf) => {
-	scene.add(gltf.scene);
+	// scene.add(gltf.scene);
 });
+
+
+const boid = makeBoid();
+const origin: THREE.Vector3 = new THREE.Vector3(0, 0, 0);
+origin.copy(boid.position);
+
+
+console.log(boid.position.distanceTo(boxMesh.position));
+
+boid.name = "boid";
+scene.add(boid);
+const direction = new THREE.Vector3();
+const left = new THREE.Vector3(-1, 0, 0);
+
+// scene.add(new THREE.ArrowHelper(direction.subVectors(left, boid.position).normalize(), boid.position, 2));
+
 
 const rendering = () => {
 	stats.begin();
 	requestAnimationFrame(rendering);
-
-	raycaster.setFromCamera(pointer, camera);
-	const intersects = raycaster.intersectObjects(scene.children, false);
-
+	// console.clear();
 	controls.update();
+	raycaster.set(boid.position, direction.subVectors(left, boid.position).normalize());
+	console.log(raycaster.intersectObjects(scene.children));
+	
 	// scene.rotation.x -= 0.01;
 	// scene.rotation.z -= 0.01;
-	scene.rotation.y -= 0.001;
+	// boid.rotation.y -= 0.01;
 	renderer.render(scene, camera);
 	stats.end();
 }
