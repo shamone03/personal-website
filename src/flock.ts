c1e45ee0b43a8a0b0fc942ae661f5764a399c9da
Aryah Kannan <68197305+shamone03@users.noreply.github.com>
2023-09-16 21:27:34 -0300

    fixed build

--- src/flock.ts
+++ src/flock.ts
@@ -2,8 +2,15 @@ import Boid from "./boid";
 
 class FlockManager {
     boids: Boid[]
-
+    perception: number;
+    separation: number;
+    alignment: number;
+    cohesion: number;
     constructor(scene: THREE.Scene, numBoids: number, perception: number, separation: number, alignment: number, cohesion: number) {
+        this.perception = perception;
+        this.alignment = alignment;
+        this.cohesion = cohesion;
+        this.separation = separation;
         this.boids = [];
         for (let i = 0; i < numBoids; i++) {
             const b = new Boid(scene);
